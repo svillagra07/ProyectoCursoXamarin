977d564e87ae2ca850cac5ea0c81e5cc95e6980b
Samuel Villagra <svillagra07@gmail.com>
2018-06-27 10:05:25 -0600

    Inserción inicial en el repositorio del módulo AppBancoUniversal

---
+++ AppBancoUniversal/XAML/RegistroPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace AppBancoUniversal.XAML
+{
+    public partial class RegistroPage : ContentPage
+    {
+        public RegistroPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
